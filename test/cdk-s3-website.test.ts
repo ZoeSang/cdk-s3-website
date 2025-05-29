5b2ab400c23065b2de9ca4956047cfd4ff808f79
Zoe Sang <ziyesang@amazon.com>
2025-05-29 16:43:13 -0700

    fix failed test

--- test/cdk-s3-website.test.ts
+++ test/cdk-s3-website.test.ts
@@ -1,6 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as CdkS3Website from '../lib/cdk-s3-website-stack';
+import * as fs from 'fs';
+import * as path from 'path';
+
+beforeAll(() => {
+    // Create the dist directory if it doesn't exist
+    const distPath = path.join(__dirname, '../website/dist');
+    if (!fs.existsSync(distPath)) {
+        fs.mkdirSync(distPath, { recursive: true });
+        // Create a dummy index.html file
+        fs.writeFileSync(path.join(distPath, 'index.html'), '<html><body>Test</body></html>');
+    }
+});
 
 test('S3 Bucket Created With Website Configuration', () => {
     const app = new cdk.App();
